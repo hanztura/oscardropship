418b534db196892a459eed70172c9cf8731d877e
hanz <hcturaofficial@bitbucket.org>
2020-08-27 20:13:19 +0800

    best sellers, most viewed and featured products in home page.

--- oscardropship/wagtail_pages/models.py
+++ oscardropship/wagtail_pages/models.py
@@ -12,6 +12,7 @@ from .blocks import BannerBlock
 
 Category = get_model('catalogue', 'Category')
 Product = get_model('catalogue', 'Product')
+ProductRecord = get_model('analytics', 'ProductRecord')
 
 
 class HomePage(Page):
@@ -33,16 +34,39 @@ class HomePage(Page):
             'special_products',
             label='Special Products',
         ),
+        InlinePanel(
+            'featured_products',
+            label='Featured Products',
+        ),
     ]
 
     def get_template(request, *args, **kwargs):
         return 'uikit/home.html'
 
+    def get_most_viewed_products(self):
+        queryset = ProductRecord.objects.filter(product__parent__isnull=True)
+        queryset = queryset.prefetch_related('product__images')
+        queryset = queryset.order_by('-num_views')[:10]
+        return queryset
+
+    def get_best_selling_products(self):
+        queryset = ProductRecord.objects.filter(product__parent__isnull=True)
+        queryset = queryset.prefetch_related('product__images')
+        queryset = queryset.order_by('-num_purchases', '-num_views')[:10]
+        return queryset
+
+    def get_newly_arrived_products(self):
+        queryset = Product.objects.browsable().filter(parent__isnull=True)
+        queryset = queryset.prefetch_related('images')[:6]
+        return queryset
+
     def get_context(self, request):
         context = super().get_context(request)
 
         # Add extra variables and return the updated context
-        context['new_arrivals'] = Product.objects.browsable()[:6]
+        context['new_arrivals'] = self.get_newly_arrived_products()
+        context['most_viewed_products'] = self.get_most_viewed_products()
+        context['best_selling_products'] = self.get_best_selling_products()
         return context
 
 
@@ -69,7 +93,22 @@ class HomePageSpecialProduct(Orderable, models.Model):
     product = models.ForeignKey(
         Product,
         on_delete=models.CASCADE,
-        related_name='in_home_page')
+        related_name='home_page_specials')
+
+    panels = [
+        FieldPanel('product')
+    ]
+
+
+class HomePageFeaturedProduct(Orderable, models.Model):
+    page = ParentalKey(
+        'wagtail_pages.HomePage',
+        on_delete=models.CASCADE,
+        related_name='featured_products')
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='home_page_featured')
 
     panels = [
         FieldPanel('product')
